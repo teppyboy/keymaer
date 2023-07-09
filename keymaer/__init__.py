eae9302b2ea94738a1b56e0087e0b6d6fadd133f
tretrauit <tretrauit@gmail.com>
2023-07-10 04:34:43 +0700

    chore: move optimization code to init

--- keymaer/__init__.py
+++ keymaer/__init__.py
@@ -1,4 +1,11 @@
-from keymaer.engine import Delay, KeyMap
+import pyjion
+pyjion.config(level=2, pgc=False)
+if pyjion.enable():
+    print("EXPERIMENTAL: Pyjion optimization enabled")
+    print("Pyjion may takes up to 30 seconds to properly optimize the code.")
+    print("During JIT optimization, your system will be unresponsive.")
+
+from keymaer.engine import Delay, KeyMap  # noqa: E402
 
 
 __all__ = ["Delay", "KeyMap"]
--- keymaer/__main__.py
+++ keymaer/__main__.py
@@ -1,9 +1,7 @@
 import pyjion
-pyjion.config(level=2, pgc=False)
-if pyjion.enable():
-    print("EXPERIMENTAL: Pyjion optimization enabled")
-    print("Pyjion may takes up to 30 seconds to properly optimize the code.")
-    print("During JIT optimization, your system will be unresponsive.")
+
 from .app import main  # noqa: E402
+
+
 print(vars(pyjion.info(main)))
 main()
